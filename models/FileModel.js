810ff6894eec090e8ca669651c17d6d911872a11
onkar-giram <meonkargiram@gmail.com>
2023-11-08 00:17:14 +0530

    Mail sending service implemented

--- models/FileModel.js
+++ models/FileModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -14,6 +15,28 @@ const fileSchema = new mongoose.Schema({
   email: {
     type: String,
   },
+});
+
+//post middleware
+fileSchema.post("save", async function (doc) {
+  try {
+    let transporter = nodemailer.transporter({
+      hsot: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: "Onkar Giram",
+      to: doc.email,
+      subject: "New File Uploaded on Cloudinary",
+      html: `<h2> Hello </h2> <p>Your File is not Uploaded on Cloudinary </p> View here : <a href="${doc.imageUrl}"> ${doc.imageUrl}</a>`,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 module.exports = mongoose.model("File", fileSchema);

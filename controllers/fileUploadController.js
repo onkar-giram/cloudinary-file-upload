abaaf6c4d5796c4f03ee146239f9708391316ef8
onkar-giram <meonkargiram@gmail.com>
2023-11-07 20:57:13 +0530

    reduced/compressed image upload complete

--- controllers/fileUploadController.js
+++ controllers/fileUploadController.js
@@ -6,8 +6,13 @@ function isFileSupported(type, supportedTypes) {
   return supportedTypes.includes(type);
 }
 
-async function uploadFileToCloudinary(file, folder) {
+async function uploadFileToCloudinary(file, folder, quality) {
   const options = { folder };
+
+  if (quality) {
+    options.quality = quality;
+  }
+
   options.resource_type = "auto";
   return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
@@ -97,7 +102,49 @@ exports.videoUpload = async (req, res) => {
     });
   }
 };
-exports.imageReducerUpload = async (req, res) => {};
+
+exports.imageReducerUpload = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+
+    const file = req.files.imageFile;
+    console.log(file);
+
+    // validation
+    const supportedTypes = ["jpg", "jpeg", "png"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    if (!isFileSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File format not suppported",
+      });
+    }
+
+    const response = await uploadFileToCloudinary(file, "demo", 30);
+    console.log(response);
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.json({
+      success: true,
+      image_url: response.secure_url,
+      message: " Image successfully uploaded",
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: " kuch to hua hai",
+    });
+  }
+};
+
 exports.localFileUpload = async (req, res) => {
   try {
     const file = req.files.file;
